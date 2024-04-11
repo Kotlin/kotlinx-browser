e26e696ca33ecfb247f6b950c741bc6f91b996ba
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2024-04-11 16:55:18 +0200

    Generate kotlin declarations from idl's

---
+++ src/wasmJsMain/kotlin/org.w3c/org.w3c.dom.parsing.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+// NOTE: THIS FILE IS AUTO-GENERATED, DO NOT EDIT!
+// See github.com/kotlin/dukat for details
+
+package org.w3c.dom.parsing
+
+import kotlin.js.*
+import org.khronos.webgl.*
+import org.w3c.dom.*
+import org.w3c.undefined
+
+/**
+ * Exposes the JavaScript [DOMParser](https://developer.mozilla.org/en/docs/Web/API/DOMParser) to Kotlin
+ */
+public external open class DOMParser : JsAny {
+    fun parseFromString(str: String, type: JsAny?): Document
+}
+
+/**
+ * Exposes the JavaScript [XMLSerializer](https://developer.mozilla.org/en/docs/Web/API/XMLSerializer) to Kotlin
+ */
+public external open class XMLSerializer : JsAny {
+    fun serializeToString(root: Node): String
+}
